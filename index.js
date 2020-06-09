a73ae4ef823e2e30a550bd9ac5d17ce834fcf8a7
welshman <welshman@users.noreply.github.com>
2020-06-09 07:19:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,5 +26,6 @@ results += randomizer[Math.floor(Math.random() * randomizer.length)];
 return results;
 
 }
+if(message.content === '>discord') return message.channel.send("https://discord.gg/SuFN7eD");
 if(message.content === '>nitro') return message.channel.send("https:/"+"/discord.gift/"+ randomizer(16,'0aA'));
 });
