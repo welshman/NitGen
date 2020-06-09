76747805434096b4a0288936cb2473ea2023a8b6
welshman <welshman@users.noreply.github.com>
2020-06-09 03:28:27 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js'); 
+const client = new Discord.Client();
+const config = require("./config.json")
+
+client.login(config.token);
+
+client.on ('ready', async () =>{
+	console.log(`Bot Is Online`)
+})
+
+client.on('message', message => { 
+    function randomizer(length, letter) {
+
+var randomizer = '';
+if (letter.indexOf('0') > -1) randomizer += '0123456789';
+if (letter.indexOf('A') > -1) randomizer += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+if (letter.indexOf('a') > -1) randomizer += 'abcdefghijklmnopqrstuvwxyz';
+var results = '';
+
+
+for (var i = length; i > 0; --i) 
+{
+results += randomizer[Math.floor(Math.random() * randomizer.length)];
+
+}
+
+return results;
+
+}
+if(message.content === '>nitro') return message.channel.send("https:/"+"/discord.gift/"+ randomizer(16,'0aA'));
+});
